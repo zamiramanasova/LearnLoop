c5b552a84b73994c19fdc718c8f5c4b275359663
Zamira <keldibaevaz@gmail.com>
2026-03-17 23:42:24 +0600

    refactoring

--- src/test/java/com/example/usefy/UsefyApplicationTests.java
+++ src/test/java/com/example/usefy/UsefyApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.usefy;
 
 import com.example.usefy.service.ai.AiService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
